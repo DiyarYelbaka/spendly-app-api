8e7be5117a1476cd18e76074b1cbc3c3de2ef098
Diyar Yelbaka <103632571+DiyarYelbaka@users.noreply.github.com>
2025-11-18 00:24:59 +0300

    update

--- prisma/migrations/20251117205455_init/migration.sql
+++ prisma/migrations/20251117205455_init/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "users" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
+    "password" TEXT,
     "name" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
